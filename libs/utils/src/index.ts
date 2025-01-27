7dab3d0cef72924728fa5c120637af3e61d6cf16
ohabash <contactomarnow@gmail.com>
2025-01-27 16:44:53 -0600

    env-test

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/utils';
-export * from './lib/environment';
+// export * from './lib/environment';
 export * from './lib/error-handler';
 export * from './lib/functions';
 export * from './lib/errorhandler.interface';
