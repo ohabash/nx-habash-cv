66529207d6000d3fd1b2a0111a9cb6c6a04bae2e
ohabash <contactomarnow@gmail.com>
2025-01-27 16:41:24 -0600

    secure-env-vars

--- apps/next-folio/src/app/firebase/firebase.config.ts
+++ apps/next-folio/src/app/firebase/firebase.config.ts
@@ -1,10 +1,18 @@
-import { environment } from '@nx-habash/utils';
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { RTDB } from './rtdb.firebase';
 
 export * from './errors.firebase';
-export const firebaseConfig = { ...environment().fb, };
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FB_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FB_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FB_projectId,
+  databaseURL: process.env.NEXT_PUBLIC_FB_databaseURL,
+  storageBucket: process.env.NEXT_PUBLIC_FB_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FB_appId,
+  measurementId: process.env.NEXT_PUBLIC_FB_measurementId,
+};
 
 // initialize firebase app
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
