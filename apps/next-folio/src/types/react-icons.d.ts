1e90926181be4e2f07f675f316e64f1ab0bba46d
ohabash <contactomarnow@gmail.com>
2025-01-31 11:06:42 -0600

    fixed build issues

--- apps/next-folio/src/types/react-icons.d.ts
+++ apps/next-folio/src/types/react-icons.d.ts
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
-import { IconBaseProps } from 'react-icons';
+import { ReactNode } from "react";
+import { IconBaseProps } from "react-icons";
 
-declare module 'react-icons/lib/index' {
+declare module "react-icons/lib/index" {
   export type IconType = (props: IconBaseProps) => ReactNode;
 }
--- react-icons.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { IconBaseProps } from "react-icons";
-
-declare module "react-icons/lib/index" {
-  export type IconType = (props: IconBaseProps) => ReactNode;
-}
