eb217e2960a4d26eef14c59d45f73f63b1771cfb
ohabash <contactomarnow@gmail.com>
2025-07-16 13:18:28 -0600

    ai bug fix

--- apps/next-folio/src/app/components/copilotKit/components/AiBody.tsx
+++ apps/next-folio/src/app/components/copilotKit/components/AiBody.tsx
@@ -1,26 +1,24 @@
 'use client';
 import React from 'react';
-import { Suggestions } from './Suggestions';
 import { useChatContext } from '@copilotkit/react-ui';
-import { useCopilotProfessionalContext } from '../useReadable';
-import { useCopilotActions } from '../useActions';
-import { INSTRUCTIONS } from '../useReadable';
+
 
 export const AiBody: React.FC = () => {
   const { open, setOpen } = useChatContext();
+  const active = false;
   
   // Initialize professional context and actions
-  useCopilotProfessionalContext();
-  useCopilotActions();
+  // useCopilotProfessionalContext();
+  // useCopilotActions();
 
   return (
     <>
-      {open && (
+      {(open && active) && (
         <div
           id="ai-body"
           className="z-50 blurrr h-screen overflow-hidden sticky top-0 w-full slideInLeft animated"
         >
-          <Suggestions />
+          {/* <Suggestions /> */}
         </div>
       )}
     </>
